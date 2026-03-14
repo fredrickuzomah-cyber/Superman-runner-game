eda3225497e48c394b604e34dfe3ee470ff2b8bc
fredrickuzomah-cyber <fredrick.uzomah@gmail.com>
2026-03-14 12:56:59 +0000

    Add files via upload

--- supermanrunner.py
+++ supermanrunner.py
@@ -1,11 +1,25 @@
 import pyautogui,random,pgzrun
+
+
 WIDTH,HEIGHT=pyautogui.size()
+
+obstacles=["explosion","alien_ship","meteor"]
+enemies=[]
+
+def create_obstacles():
+    obstacle=Actor(random.choice(obstacles))
+    obstacle.pos=WIDTH,random.randint(0,HEIGHT)
+    enemies.append(obstacle)
 
 superman=Actor("superman.png")
 superman.pos=90,HEIGHT/2
 superman.angle=90
+
 def draw():
+    screen.clear()
     superman.draw()
+    for e in enemies:
+        e.draw()
 
 def update():
     if keyboard.up:
@@ -16,6 +30,10 @@ def update():
         superman.y=HEIGHT
     if superman.y>HEIGHT:
         superman.y=0
+    for i in enemies:
+        i.x-=10
 
+
+clock.schedule_interval(create_obstacles,1)
 
 pgzrun.go()
