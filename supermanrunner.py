4e5f2cf51cf12e818148092e6ae57bd108e86330
fredrickuzomah-cyber <fredrick.uzomah@gmail.com>
2026-03-28 12:50:16 +0000

    Add files via upload

--- supermanrunner.py
+++ supermanrunner.py
@@ -1,13 +1,17 @@
 import pyautogui,random,pgzrun
 
 
+
 WIDTH,HEIGHT=pyautogui.size()
 
 obstacles=["explosion","alien_ship","meteor"]
 enemies=[]
+lasers=[]
 
 def create_obstacles():
     obstacle=Actor(random.choice(obstacles))
+    if obstacle.image=="meteor":
+        obstacle.right=-45
     obstacle.pos=WIDTH,random.randint(0,HEIGHT)
     enemies.append(obstacle)
 
@@ -16,11 +20,12 @@ superman.pos=90,HEIGHT/2
 superman.angle=90
 
 def draw():
-    screen.clear()
+    screen.blit("skybackground.jpg",(0,0))
     superman.draw()
     for e in enemies:
         e.draw()
-
+    for e in lasers:
+        e.draw()
 def update():
     if keyboard.up:
         superman.y-=10
@@ -32,8 +37,22 @@ def update():
         superman.y=0
     for i in enemies:
         i.x-=10
+        if i.x<0:
+            enemies.remove(i)
+    for i in lasers:
+        i.x+=10
+        if i.x > WIDTH:
+            lasers.remove(i)
+        for e in enemies:
+            if i.colliderect(e):
+                lasers.remove(i)
+                enemies.remove(e)
+def on_key_down(key):
+    if key==keys.SPACE:
+        laser=Actor("redbeam.png")
+        laser.pos=superman.x-20,superman.y-200
+        lasers.append(laser)
 
-
-clock.schedule_interval(create_obstacles,1)
+clock.schedule_interval(create_obstacles,4)
 
 pgzrun.go()
